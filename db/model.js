a0e455c3ad0f4c13b23737837790e322205fe726
victor <viktormiene.vm@gmail.com>
2022-03-22 19:45:52 +0100

    feat: add mongoose config and book model

---
+++ db/model.js
@@ -0,0 +1,22 @@
+import * as mongoose from "mongoose";
+
+
+const BookSchema = new mongoose.Schema({
+  isbn: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  title: {
+    type: String,
+    required: true
+    },
+  author: {
+    type: String,
+    required: true
+}
+});
+
+
+const Books = mongoose.model("Book", BookSchema);
+export default Books;
